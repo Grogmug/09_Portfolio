f0a902eacfadfea32d598add96c67267c3fb83a7
Søren <Sren@DESKTOP-3DFGV3A>
2016-09-27 09:02:24 +0200

    Add project files.

---
+++ 09_Portfolio/Portfolio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace _09_Portfolio
+{
+    internal class Portfolio
+    {
+        List<IAsset> items;
+
+        public Portfolio ()
+        {
+            items = new List<IAsset>();
+        }
+        public Portfolio(List<IAsset> items)
+        {
+            this.items = items;
+        }
+
+        public double GetTotalValue()
+        {
+            double fullValue = 0;
+            foreach (IAsset item in items)
+            {
+                fullValue += item.GetValue();
+            }
+            return fullValue;
+        }
+
+        public void AddAsset(IAsset listId)
+        {
+            items.Add(listId);
+        }
+    }
+}
---
+++ 09_Portfolio/SavingsAccount.cs
@@ -0,0 +1,34 @@
+﻿namespace _09_Portfolio
+{
+    internal class SavingsAccount : IAsset
+    {
+        string name;
+        double value, interestRate;
+        public SavingsAccount(string name, double value,double interestRate)
+        {
+            this.name = name;
+            this.value = value;
+            this.interestRate = interestRate;
+        }
+
+        public double GetValue()
+        {
+            return value;
+        }
+
+        public double InterestRate
+        {
+            get { return interestRate; }
+        }
+
+        public override string ToString()
+        {
+            return "SavingsAccount[value=" + value + ".0,interestRate=" + interestRate.ToString().Replace(',','.')+ "]";
+        }
+
+        public void ApplyInterest()
+        {
+            value *= 1 + (interestRate / 100);
+        }
+    }
+}
